e433c6d54e91dd140912ea7a12be00bfc0af4626
wagner1986 <rengaw.luiz@gmail.com>
2012-07-07 05:47:38 -0300

    projeto construido no rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redesocial::Application.config.secret_token = '829ff938e447425f57f691574fc7e0131522f660c090d9031854d6f0d7c11cb81393ec6a21e02e873cf4e5e921383fc9dde6c84c40eb7d6ab1387e796af04a1b'
